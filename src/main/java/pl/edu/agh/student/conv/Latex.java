9a52cf6fe05f247bbaedfc205da94552a55e46bd
Jakub Pelc <jakubspelc@gmail.com>
2014-06-06 10:20:21 +0200

    location of generated sources changed

--- src/main/java/pl/edu/agh/student/conv/Latex.java
+++ src/main/java/pl/edu/agh/student/conv/Latex.java
@@ -6,17 +6,18 @@ import java.io.IOException;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import pl.edu.agh.student.jpelc.conv.LatexLexer;
-import pl.edu.agh.student.jpelc.conv.LatexParser;
+import pl.edu.agh.student.conv.generated.LatexLexer;
+import pl.edu.agh.student.conv.generated.LatexParser;
 
 public class Latex {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException {
 		LatexLexer lexer = new LatexLexer(new ANTLRFileStream(args[0]));
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		LatexParser parser = new LatexParser(tokens);
 		parser.setBuildParseTree(true);
-		
+
 	}
 
 }
